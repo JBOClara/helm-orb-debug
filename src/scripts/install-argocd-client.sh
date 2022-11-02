1a198df2b7eb28e2c72f10b68ae6eabee1d6fd60
jboulanger <julien.boulanger@fr.clara.net>
2022-11-02 19:39:34 +0100

    feat(ci): wip

--- src/scripts/install-argocd-client.sh
+++ src/scripts/install-argocd-client.sh
@@ -5,7 +5,7 @@ if command -v argocd &> /dev/null; then
     exit 0
 fi
 
-BINARY_URL="https://github.com/argoproj/argo-cd/releases/download/$VERSION/argocd-linux-amd64"
+BINARY_URL="https://github.com/argoproj/argo-cd/releases/download/$ARGO_CLI_VERSION/argocd-linux-amd64"
 
 curl -sSL -o argocd-linux-amd64 "${BINARY_URL}"
 sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
